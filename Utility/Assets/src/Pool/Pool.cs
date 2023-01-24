398968198e6bcb4954f8a9c571b29cc996004a75
Djason NATHIEZ <60692459+DjasonNathiez@users.noreply.github.com>
2023-01-24 12:36:11 +0100

    version update push

---
+++ Utility/Assets/src/Pool/Pool.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> where T : Object
+{
+    private readonly Queue<T> pool = new Queue<T>();
+    private readonly T defaultItem;
+
+    public Pool(T defaultItem)
+    {
+        this.defaultItem = defaultItem;
+    }
+
+    public void AddToPool(T toAdd)
+    {
+        if (toAdd is GameObject t)
+        {
+            t.SetActive(false);
+        }
+        pool.Enqueue(toAdd);
+    }
+
+    public T GetFromPool()
+    {
+        return pool.Count > 0 ? pool.Dequeue() : Object.Instantiate(defaultItem);
+    }
+
+    public IEnumerator AddToPoolLatter(T item, float lifeTime)
+    {
+        yield return new WaitForSeconds(lifeTime);
+        AddToPool(item);
+    }
+}
--- Utility/Assets/testlog.cs
+++ Utility/Assets/testlog.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class testlog : MonoSingleton<testlog>
 {
     public Transform cube;
+    public GameObject prefabToPool;
     
     void Start()
     {
+        var pool = new Pool<GameObject>(prefabToPool);
+        var obj = pool.GetFromPool();
+        pool.AddToPool(obj);
+        pool.AddToPool(obj);
+        //StartCoroutine(pool.AddToPoolLatter(obj, 5f));
+        
         Logs.Log("WESH", "BONJOUR", LogType.Log, Logs.LogColor.Green, Logs.LogColor.Red);
         
         cube.SetPosX(2);
