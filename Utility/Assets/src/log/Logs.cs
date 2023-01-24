aa3d3e43e7c2b99d875e6ca6843d6e267d59ceec
Djason NATHIEZ <60692459+DjasonNathiez@users.noreply.github.com>
2023-01-24 11:43:21 +0100

    update white color

--- Utility/Assets/src/log/Logs.cs
+++ Utility/Assets/src/log/Logs.cs
@@ -5,14 +5,50 @@ using Debug = UnityEngine.Debug;
 public class Logs : MonoBehaviour
 {
     [Conditional("DEBUG")]
-    public static void Log(string text)
+    public static void Log(string title, string log, LogType logType = LogType.Log, LogColor titleColor = LogColor.None, LogColor logColor = LogColor.None)
     {
-        Debug.Log(text);
+        title = $"<b>[{title}]</b>";
+
+        if (titleColor != LogColor.None)
+        {
+            title = $"<color={GetColorByLogColor(titleColor)}>{title}</color>";
+        }
+
+        if (logColor != LogColor.None)
+        {
+            log = $"<color={GetColorByLogColor(titleColor)}>{log}</color>";
+        }
+        
+        Debug.unityLogger.Log(logType, $"{title} -- {log}");
     }
 
-    [Conditional("DEBUG")]
-    public static void LogError(string text)
+    public static string GetColorByLogColor(LogColor logColor)
     {
-        Debug.LogError(text);
+        string color = "0,0,0";
+
+        switch (logColor)
+        {
+            case LogColor.Blue:
+                return ":blue";
+            
+            case LogColor.Green:
+                return ":green";
+            
+            case LogColor.Red:
+                return ":red";
+
+            case LogColor.None:
+                return ":white";
+        }
+        
+        return ":white";
+    }
+
+    public enum LogColor
+    {
+        None,
+        Red,
+        Blue,
+        Green
     }
 }
