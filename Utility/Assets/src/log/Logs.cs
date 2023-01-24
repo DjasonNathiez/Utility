08ca593424407c7e315c353d779234c10af49d87
Djason NATHIEZ <60692459+DjasonNathiez@users.noreply.github.com>
2023-01-24 11:01:15 +0100

    Utility update with log function

---
+++ Utility/Assets/src/log/Logs.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Logs : MonoBehaviour
+{
+    [Conditional("DEBUG")]
+    public static void Log(string text)
+    {
+        Debug.Log(text);
+    }
+}
